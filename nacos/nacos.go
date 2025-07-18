600b1709813c3025538cf6e683e4d1c0de3c5f7c
13333911434 <876986347@qq.com>
2025-07-18 21:51:44 +0800

    Merge remote-tracking branch 'origin/mkxz' into mkxz

--- nacos/nacos.go
+++ nacos/nacos.go
@@ -2,12 +2,18 @@ package nacos
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/nacos-group/nacos-sdk-go/v2/clients"
 	"github.com/nacos-group/nacos-sdk-go/v2/common/constant"
 	"github.com/nacos-group/nacos-sdk-go/v2/vo"
 )
 
+type Nacos struct {
+	NamespaceId string
+	Group       string
+	DataId      string
+	Port        int
+	IpAddr      string
+}
 type T struct {
 	Mysql struct {
 		Addr     string `json:"addr"`
@@ -32,45 +38,40 @@ type T struct {
 	} `json:"aliyun"`
 }
 
-var ns T
+var (
+	Configs  T
+	NacosSrv Nacos
+)
 
-func Naocs(namespaceId, IpAddr, DataId, Group string, Port int) {
-	//create clientConfig
+func NacosInit(namespaceId, ipAddr, dataId, group string, port uint64) (T, Nacos) {
 	clientConfig := constant.ClientConfig{
-		NamespaceId:         namespaceId, //we can create multiple clients with different namespaceId to support multiple namespace.When namespace is public, fill in the blank string here.
+		NamespaceId:         namespaceId, // 如果需要支持多namespace，我们可以创建多个client,它们有不同的NamespaceId。当namespace是public时，此处填空字符串。
 		TimeoutMs:           5000,
 		NotLoadCacheAtStart: true,
 		LogDir:              "/tmp/nacos/log",
 		CacheDir:            "/tmp/nacos/cache",
 		LogLevel:            "debug",
 	}
-	// At least one ServerConfig
 	serverConfigs := []constant.ServerConfig{
 		{
-			IpAddr:      IpAddr,
+			IpAddr:      ipAddr,
 			ContextPath: "/nacos",
-			Port:        uint64(Port),
+			Port:        port,
 			Scheme:      "http",
 		},
 	}
-	// Create config client for dynamic configuration
-	clients.CreateConfigClient(map[string]interface{}{
+	configClient, err := clients.CreateConfigClient(map[string]interface{}{
 		"serverConfigs": serverConfigs,
 		"clientConfig":  clientConfig,
 	})
-	// Another way of create config client for dynamic configuration (recommend)
-	configClient, _ := clients.NewConfigClient(
-		vo.NacosClientParam{
-			ClientConfig:  &clientConfig,
-			ServerConfigs: serverConfigs,
-		},
-	)
-	content, _ := configClient.GetConfig(vo.ConfigParam{
-		DataId: DataId,
-		Group:  Group})
-	err := json.Unmarshal([]byte(content), &ns)
 	if err != nil {
-		return
+		return T{}, NacosSrv
 	}
-	fmt.Println(content)
+	content, err := configClient.GetConfig(vo.ConfigParam{
+		DataId: dataId,
+		Group:  group})
+	configAdd := T{}
+	json.Unmarshal([]byte(content), &configAdd)
+	Configs = configAdd
+	return Configs, NacosSrv
 }
